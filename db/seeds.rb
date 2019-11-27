45a35afc9cbfe340a7e9cc49415753e2ff3a5ee1
gasheibrahim <ibrahimgashema154@gmail.com>
2019-11-27 12:24:38 +0200

    correction of coflict of versions

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Property.create(property: 'House', rent: 20000, address: "Kigali", buildingAge: '20', remarks: "The house is great", rail_line: 'kgl 230A', station_name: 'kgl', number_of_minutes: '20', rail_line1: 'Adali 12', station_name1: 'ADB 12', number_of_minutes1: '30')

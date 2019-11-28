eeed7b8202703ba8b642a89799b4a789f295fef5
gasheibrahim <ibrahimgashema154@gmail.com>
2019-11-28 14:30:36 +0200

    collection of undefined methods

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,9 +2,5 @@ class Property < ApplicationRecord
     validates :property, :rent, :address, :buildingAge, :remarks, :rail_line, 
               :station_name, :number_of_minutes, :rail_line1, :station_name1,
               :number_of_minutes1, presence: true
-              if number_of_minutes.present? && number_of_minutes1.present?
-			  if number_of_minutes <= number_of_minutes1
-				errors.add(:How_many_minutes_walk, "please check again how it works")
-			  end
 end
 end
